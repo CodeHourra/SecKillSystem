420f4a55b839745cdc09b0d227153c134c1744b1
liugan <925060392@qq.com>
2019-02-13 21:01:39 +0800

    chore： 新增validation校验方法

--- src/main/java/com/miaoshaproject/validator/ValidationResult.java
+++ src/main/java/com/miaoshaproject/validator/ValidationResult.java
@@ -1,10 +1,48 @@
 package com.miaoshaproject.validator;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author liugan83@gmail.com
  * @version V1.0
  * @date 2019/2/12 20:50
- * @Description TODO
+ * @Description 自定义校验封装类
  */
 public class ValidationResult {
+  /**
+   * 校验结果是否有错
+   */
+  private boolean hasErrors = false;
+
+  /**
+   * 存放错误信息
+   */
+  private Map<String, String> errorMsgMap = new HashMap<>();
+
+  public boolean isHasErrors() {
+    return hasErrors;
+  }
+
+  public void setHasErrors(boolean hasErrors) {
+    this.hasErrors = hasErrors;
+  }
+
+  public Map<String, String> getErrorMsgMap() {
+    return errorMsgMap;
+  }
+
+  public void setErrorMsgMap(Map<String, String> errorMsgMap) {
+    this.errorMsgMap = errorMsgMap;
+  }
+
+  /**
+   * 通用的格式化字符串信息返回错误结果的message的方法
+   * @return
+   */
+  public String getErrorMsg() {
+    return StringUtils.join(errorMsgMap.values().toArray(), ",");
+  }
 }
--- src/main/java/com/miaoshaproject/validator/ValidatorImpl.java
+++ src/main/java/com/miaoshaproject/validator/ValidatorImpl.java
@@ -1,10 +1,47 @@
 package com.miaoshaproject.validator;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Set;
+
 /**
  * @author liugan83@gmail.com
  * @version V1.0
  * @date 2019/2/12 20:56
  * @Description TODO
  */
-public class ValidatorImpl {
+@Component
+public class ValidatorImpl implements InitializingBean {
+
+  private Validator validator;
+
+  /**
+   * 实现校验方法并返回校验结果
+   * @param bean
+   * @return
+   */
+  public ValidationResult validationResult(Object bean) {
+    final ValidationResult result = new ValidationResult();
+    Set<ConstraintViolation<Object>> validate = validator.validate(bean);
+    if (validate.size() > 0) {
+      // hasError
+      result.setHasErrors(true);
+      validate.forEach(constraintViolation -> {
+        String errMsg = constraintViolation.getMessage();
+        String propertyName = constraintViolation.getPropertyPath().toString();
+        result.getErrorMsgMap().put(propertyName, errMsg);
+      });
+    }
+    return result;
+  }
+
+  @Override
+  public void afterPropertiesSet() throws Exception {
+    // 将hibernate validator通过工厂的初始化方式使其实例化
+    this.validator = Validation.buildDefaultValidatorFactory().getValidator();
+  }
 }
